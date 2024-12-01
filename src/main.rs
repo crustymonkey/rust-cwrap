13ffd12ed88068477c46eb0ec5a1ed90cf699170
Jay Deiman <jay@splitstreams.com>
2024-12-01 12:04:34 -0800

    Organized the CLI options

--- src/main.rs
+++ src/main.rs
@@ -74,70 +74,69 @@ struct Args {
     #[arg(short = 'z', long, default_value_t = 0)]
     fuzz: usize,
     /// Only output error reports. If the command runs successfully,
-    /// command runs successfully, nothing will be printed, even if
-    /// the command had stdout or stderr output.
+    /// nothing will be printed, even if the command had stdout or stderr output.
     #[arg(short, long)]
     quiet: bool,
     /// If this is set, it will log *all* failures to syslog.
     /// This is useful for diagnosing intermittent failures that don't
     /// necessarily trip the number of failures for a report
-    #[arg(short = 'S', long)]
+    #[arg(short = 'S', long, help_heading = "SYSLOG")]
     syslog: bool,
     /// Set the logging facility.  The list of available facilities is here: http://t.ly/2nqs
-    #[arg(short = 'C', long = "syslog_facility", default_value = "log_local7")]
+    #[arg(short = 'C', long = "syslog_facility", help_heading = "SYSLOG", default_value = "log_local7")]
     syslog_fac: String,
     /// Set the syslog priority
-    #[arg(short = 'P', long = "syslog_priority", default_value = "log_info")]
+    #[arg(short = 'P', long = "syslog_priority", help_heading = "SYSLOG", default_value = "log_info")]
     syslog_pri: String,
     /// Send an email directly from within cwrap itself.  This option is *required*
     /// with any of the SMTP options below this.  If this is not specified, any
     /// email options below will be ignored.  Note that this can be used with
     /// --also-normal-output to also output to stdout (default: email only).
-    #[arg(short = 'M', default_value_t = false)]
+    #[arg(short = 'M', long, help_heading = "EMAIL")]
     send_mail: bool,
     /// If specified along with --send-mail, cwrap will output to stdout *and*
     /// send a notification email.  The default with --send-mail is to send an
     /// email ONLY.
-    #[arg(short = 'N', default_value_t = false)]
+    #[arg(short = 'N', long, help_heading = "EMAIL")]
     also_normal_output: bool,
     /// The email address to use as the sending address.  If not specified, this
     /// will be set to the username and determined hostname.  It's advised that
     /// this is set.
-    #[arg(short = 'E')]
+    #[arg(short = 'E', long, help_heading = "EMAIL")]
     email_from: Option<String>,
     /// The recipient(s) to send the email to.  This can be specified multiple
     /// times to send to multiple addresses.
-    #[arg(short = 'R')]
+    #[arg(short = 'R', long, help_heading = "EMAIL")]
     recipient: Option<Vec<String>>,
     /// The subject to use for the email.
-    #[arg(short = 'J', default_value = "cwrap failure report")]
+    #[arg(short = 'J', long, help_heading = "EMAIL", default_value = "cwrap failure report")]
     subject: String,
     /// The SMTP server address (hostname or IP) to connect to.
-    #[arg(short = 'X', default_value = "localhost")]
+    #[arg(short = 'X', long, help_heading = "EMAIL", default_value = "localhost")]
     smtp_server: String,
     /// The SMTP port to connect to.
-    #[arg(short = 'T', default_value_t = 25)]
+    #[arg(short = 'T', long, help_heading = "EMAIL", default_value_t = 25)]
     smtp_port: usize,
     /// Encrypt the connection using SSL/TLS directly.  Note that the port you
     /// connect to should expect a TLS connection (as opposed to STARTTLS).
-    #[arg(short = 'L', default_value_t = false)]
+    #[arg(short = 'L', long = "tls", help_heading = "EMAIL")]
     tls: bool,
     /// Encrypt the connection to the server using STARTTLS.  This is highly
     /// recommended unless you are using the default localhost connection.
-    #[arg(short = 'Z', long = "starttls", default_value_t = false)]
+    #[arg(short = 'Z', long = "starttls", help_heading = "EMAIL")]
     starttls: bool,
     /// The username to use for SMTP authentication.
-    #[arg(short = 'U', long = "smtp_username")]
+    #[arg(short = 'U', long = "smtp_username", help_heading = "EMAIL")]
     username: Option<String>,
     /// The password to use for SMTP authentication.
-    #[arg(short = 'W', long = "smtp_password")]
+    #[arg(short = 'W', long = "smtp_password", help_heading = "EMAIL")]
     password: Option<String>,
     /// (Recommended) Specify the path to a a credentials file instead of
     /// specifying a username and password directly.  The file should simply
     /// have the SMTP credentials in the form of USERNAME:PASSWORD as the only
     /// contents. Note that the username/password must be utf-8 or this will
     /// crash.
-    #[arg(short = 'D', long = "smtp_creds_file")]
+    #[arg(short = 'Y', long = "smtp_creds_file", help_heading = "EMAIL")]
     creds_file: Option<PathBuf>,
     /// The command to run.  This can be a single string (enclosed in quotes)
     /// passed to bash if "-g" is set or the command and it's arguments.
